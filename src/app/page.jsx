7925c767953a9755535afc7d5bf1cc8c0abb5a29
Tanmay Islam <tanmayislam101@gmail.com>
2025-08-21 11:38:59 +0600

    initial set-up v2

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Food Cart</h1>
+      <p>Your one-stop shop for delicious meals!</p>
+    </div>
+  );
+}
+
+export default HomePage;
