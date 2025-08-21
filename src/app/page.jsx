3de7a7c95ff893bb54aedc6e2b932c1630ac4915
Tanmay Islam <tanmayislam101@gmail.com>
2025-08-21 12:24:00 +0600

    added hero section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
+import Hero from '@/Components/Home/Hero';
 import React from 'react';
 
 function HomePage() {
   return (
     <div>
-      <h1>Welcome to the Food Cart</h1>
-      <p>Your one-stop shop for delicious meals!</p>
+      <Hero />
     </div>
   );
 }
