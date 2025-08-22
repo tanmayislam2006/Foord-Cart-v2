e342e82672b7f72af032ae088e249fcb0881ad3e
Tanmay Islam <tanmayislam101@gmail.com>
2025-08-22 10:38:11 +0600

    complete the mange page dashboard

--- src/app/api/menu/[id]/route.js
+++ src/app/api/menu/[id]/route.js
@@ -1,10 +1,9 @@
-
 import clientPromise from "@/lib/mongodb";
 import { ObjectId } from "mongodb";
 import { NextResponse } from "next/server";
 
-export async function GET(request,{params}) {
-    const { id } = await params;
+export async function GET(request, { params }) {
+  const { id } = await params;
   try {
     const client = await clientPromise;
     const db = client.db("foodCartUser");
@@ -13,6 +12,26 @@ export async function GET(request,{params}) {
     return NextResponse.json(menu);
   } catch (e) {
     console.error(e);
-    return NextResponse.json({ error: "Unable to fetch menu" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Unable to fetch menu" },
+      { status: 500 }
+    );
+  }
+}
+// delete a dish with id
+export async function DELETE(request, { params }) {
+  const { id } = await params;
+  try {
+    const client = await clientPromise;
+    const db = client.db("foodCartUser");
+    const menuCollection = db.collection("allMenu");
+    const result = await menuCollection.deleteOne({ _id: new ObjectId(id) });
+    return NextResponse.json(result);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Unable to delete dish" },
+      { status: 500 }
+    );
   }
 }
